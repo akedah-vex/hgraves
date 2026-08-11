0737b88d7b4577783d6ff4d75e4e01cbcbb9cb40
vex <vex@vexs-MacBook-Pro-2.local>
2026-08-11 08:25:13 -0700

    add resume page

---
+++ app/resume/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Wrapper from '@/components/wrapper'
+
+const ResumePage = () => {
+    return (
+        <Wrapper>
+            <div>
+                This feature is under active development! Check back later.
+            </div>
+        </Wrapper>
+    )
+}
+
+export default ResumePage;
